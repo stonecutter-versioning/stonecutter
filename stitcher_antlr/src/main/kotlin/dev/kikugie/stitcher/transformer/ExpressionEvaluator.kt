f99167b5eea341d297e8cfc624d28b47a024d5df
kikugie <stormikiku@gmail.com>
2025-08-13 16:52:03 +0200

    Add expression evaluation support with boolean logic handling in ANTLR transformer

---
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/transformer/ExpressionEvaluator.kt
@@ -0,0 +1,33 @@
+package dev.kikugie.stitcher.transformer
+
+import dev.kikugie.stitcher.antlr.StitcherParser
+import dev.kikugie.stitcher.api.TransformParameters
+import dev.kikugie.stitcher.data.ExpressionToken
+import dev.kikugie.stitcher.util.unsupported
+import dev.kikugie.stitcher.util.verify
+
+class ExpressionEvaluator(val parameters: TransformParameters) : ExpressionToken.Visitor<Boolean> {
+    override fun visitGroup(it: ExpressionToken.Group): Boolean = it.body.accept(this)
+
+    override fun visitUnary(it: ExpressionToken.Unary): Boolean = when (it.operator.type) {
+        StitcherParser.OP_NOT -> !it.operand.accept(this)
+        else -> unsupported("Unsupported unary operator ${it.operator.typeName}")
+    }
+
+    override fun visitBinary(it: ExpressionToken.Binary): Boolean = when (it.operator.type) {
+        StitcherParser.OP_AND -> it.left.accept(this) && it.right.accept(this)
+        StitcherParser.OP_OR -> it.left.accept(this) || it.right.accept(this)
+        else -> unsupported("Unsupported binary operator ${it.operator.typeName}")
+    }
+
+    override fun visitConstant(it: ExpressionToken.Constant): Boolean {
+        val value = it.value.text
+        return verify(parameters.constants[value]) { "Undefined constant '$value'" }
+    }
+
+    override fun visitAssignment(it: ExpressionToken.Assignment): Boolean {
+        val value = it.target?.text.orEmpty()
+        val target = verify(parameters.dependencies[value]) { "Undefined dependency '$value'" }
+        return it.predicates.all { it.comparator(target, it.version) }
+    }
+}
---
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/util/ControlFlow.kt
@@ -0,0 +1,21 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package dev.kikugie.stitcher.util
+
+@DslMarker
+annotation class TerminationDSL
+
+@TerminationDSL
+inline fun unsupported(reason: String): Nothing {
+    throw UnsupportedOperationException(reason)
+}
+
+@TerminationDSL
+inline fun missing(message: String): Nothing {
+    throw NoSuchElementException(message)
+}
+
+@TerminationDSL
+inline fun <T : Any> verify(value: T?, message: () -> String): T {
+    return value ?: missing(message())
+}
