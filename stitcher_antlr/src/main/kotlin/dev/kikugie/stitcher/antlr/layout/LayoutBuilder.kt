75bfac0ba88d029a47abdf234b80810daf703c91
kikugie <stormikiku@gmail.com>
2025-08-18 22:32:28 +0200

    Add support for scope-based block handling and improve token stack updates in `LayoutBuilder`

--- stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/antlr/layout/LayoutBuilder.kt
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/antlr/layout/LayoutBuilder.kt
@@ -6,45 +6,43 @@ import dev.kikugie.stitcher.antlr.adapter.InlineCharStream
 import dev.kikugie.stitcher.antlr.adapter.InlineTokenFactory
 import dev.kikugie.stitcher.antlr.converter.DefinitionBuilder
 import dev.kikugie.stitcher.data.BlockToken
-import dev.kikugie.stitcher.util.range
-import dev.kikugie.stitcher.util.toLeaf
-import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.ParserRuleContext
-import org.antlr.v4.runtime.Token
-import org.antlr.v4.runtime.TokenStream
+import dev.kikugie.stitcher.data.ScopeType.*
+import dev.kikugie.stitcher.util.*
+import org.antlr.v4.runtime.*
 import org.antlr.v4.runtime.tree.TerminalNode
-import java.util.ArrayDeque
-import java.util.Deque
-import kotlin.text.compareTo
+import java.util.*
 
+private val SKIPPABLE_SPACES = charArrayOf(' ', '\t', '\r', '\n')
 
 internal class LayoutBuilder(private val stream: TokenStream) {
     private val blocks: Deque<BlockToken> = ArrayDeque()
+    private val walker: ScopeStackWalker = ScopeStackWalker()
 
     private val current: Token
         get() = stream.LT(1)
 
-    fun build() {
-
+    fun build(): BlockToken.Root {
+        while (stream.LA(1) != IntStream.EOF) next()
+        return BlockToken.Root(blocks.toList())
     }
 
     private fun next() = when (current.type) {
         LayoutTokens.CONTENT -> handleContent()
         LayoutTokens.COMMENT_OPEN -> handleCommentEntry()
-        else -> TODO()
+        else -> TODO("Should be impossible to trigger")
     }
 
     private fun handleContent() = advancing {
-        val last: BlockToken? = blocks.pollLast()
-        if (last !is BlockToken.Code) return content(it)
-
-        // TODO: handle block splitting
+        content(it)
+        updateStack()
     }
 
     private fun handleCommentEntry() = advancing { opener ->
         val definition = advancing { body ->
             definition(body)?.let { return@advancing it }
-            return comment(opener, body, consume())
+            comment(opener, body, consume())
+            updateStack()
+            return
         }
 
         var marker: TerminalNode
@@ -73,11 +71,7 @@ internal class LayoutBuilder(private val stream: TokenStream) {
         }
 
         advancing { code(marker, context) }
-        updateBlockStack(blocks.last() as BlockToken.Code)
-    }
-
-    private fun updateBlockStack(code: BlockToken.Code) {
-
+        updateStack()
     }
 
     private fun consume(): Token = advancing { it }
@@ -96,13 +90,17 @@ internal class LayoutBuilder(private val stream: TokenStream) {
         blocks += BlockToken.Code(marker.toLeaf(), context.accept(DefinitionBuilder))
     }
 
+    private fun updateStack() {
+        blocks.removeLast().accept(walker)
+    }
+
     private fun definition(token: Token): StitcherParser.DefinitionContext? {
         if (token.stopIndex < token.startIndex)
             return null // Empty comment body
 
         return InlineCharStream(token).use {
             when (it.LA(1).toChar()) {
-                '?', '$', '~' -> { /* continue */ }
+                '?', '$', '~' -> Unit // Continue
                 else -> return@use null
             }
 
@@ -111,6 +109,80 @@ internal class LayoutBuilder(private val stream: TokenStream) {
             }
             val parser = StitcherParser(CommonTokenStream(lexer))
             parser.definition()
+        }
+    }
+
+    private inner class ScopeStackWalker : BlockToken.Visitor<Unit> {
+        override fun visitContent(it: BlockToken.Content) = when (val last = blocks.last) {
+            is BlockToken.Code if (last.definition.type != CLOSED) -> appendContent(it, last)
+            else -> blocks += it
+        }
+
+        override fun visitComment(it: BlockToken.Comment) = when (val last = blocks.last) {
+            is BlockToken.Code if (last.definition.type != CLOSED) -> appendComment(it, last)
+            else -> blocks += it
+        }
+
+        override fun visitCode(it: BlockToken.Code) {
+            blocks += if (it.definition.closer == null) it
+            else it.copy(scope = collectScope(it))
+        }
+
+        override fun visitRoot(it: BlockToken.Root) {
+            error("Shouldn't be called on root")
+        }
+
+        private fun appendComment(it: BlockToken.Comment, last: BlockToken.Code) {
+            blocks.replaceLast(last.copy(scope = listOf(it)))
+        }
+
+        private fun appendContent(it: BlockToken.Content, last: BlockToken.Code) {
+            InlineCharStream(it).use { stream ->
+                if (!stream.skipLeadingSpaces(*SKIPPABLE_SPACES)) {
+                    // The entire block is whitespace - there's nothing to comment
+                    blocks += it; return@use
+                }
+
+                val scopeStart = stream.index() + stream.start
+                val hasMore = stream.consumeScope(last)
+                val scopeEnd = stream.index() + stream.start
+                val scopeContent =
+                    if (!hasMore) it
+                    else BlockToken.Content(scopeStart..<scopeEnd, it.source)
+                blocks.replaceLast(last.copy(scope = listOf(scopeContent)))
+
+                if (hasMore)
+                    blocks += BlockToken.Content(scopeEnd..<stream.end, it.source)
+            }
+        }
+
+        private fun collectScope(ref: BlockToken.Code): List<BlockToken> = buildList {
+            while (true) {
+                val it = blocks.removeLast()
+                if (it !is BlockToken.Code) {
+                    this += it; continue
+                }
+
+                if (it.marker.type != ref.marker.type)
+                    error("TODO: make an error for this")
+
+                if (it.definition.type != CLOSED)
+                    error("TODO: make an error for this")
+
+                break
+            }
+        }
+
+        private fun CharStream.consumeScope(code: BlockToken.Code) = when (code.definition.type) {
+            LINE -> skipWhile {
+                it != '\n' && it != '\r'
+            }
+
+            WORD -> skipWhile {
+                it !in SKIPPABLE_SPACES
+            }
+
+            else -> LA(1) != IntStream.EOF
         }
     }
 }
