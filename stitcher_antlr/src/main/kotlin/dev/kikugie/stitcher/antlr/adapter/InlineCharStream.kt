1f1d4c6e3a8bcc3a765e3658fe0dead6c68e1de8
kikugie <stormikiku@gmail.com>
2025-08-18 22:31:44 +0200

    Add constructor for `StitcherToken` support in `InlineCharStream` for broader token input handling

--- stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/antlr/adapter/InlineCharStream.kt
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/antlr/adapter/InlineCharStream.kt
@@ -1,5 +1,6 @@
 package dev.kikugie.stitcher.antlr.adapter
 
+import dev.kikugie.stitcher.data.StitcherToken
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.IntStream
 import org.antlr.v4.runtime.Token
@@ -17,6 +18,7 @@ import org.antlr.v4.runtime.misc.Interval
  */
 internal class InlineCharStream(private val host: CharStream, val start: Int, val end: Int) : AutoCloseable, CharStream by host {
     constructor(host: Token) : this(host.inputStream, host.startIndex, host.stopIndex + 1)
+    constructor(host: StitcherToken) : this(host.source, host.range.first, host.range.last + 1)
     private val marker: Int = host.mark()
     private val index: Int = host.index()
 
