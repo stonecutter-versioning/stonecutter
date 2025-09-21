c984e9674281c426c4eca420318f19ad11703cf0
kikugie <stormikiku@gmail.com>
2025-09-21 21:10:19 +0200

    Replace `LeafToken` with a data class and simplify `StitcherToken` by removing `CharStream` references

--- stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/data/LeafToken.kt
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/data/LeafToken.kt
@@ -1,14 +1,8 @@
 package dev.kikugie.stitcher.data
 
 import dev.kikugie.stitcher.antlr.StitcherParser
-import org.antlr.v4.runtime.CharStream
 
-internal sealed interface LeafToken : StitcherToken {
-    val type: Int
-    val name: String
-        get() = StitcherParser.VOCABULARY.getDisplayName(type)
-
+internal data class LeafToken(val type: Int, override val range: IntRange, override val text: String) : StitcherToken {
+    val name: String get() = StitcherParser.VOCABULARY.getDisplayName(type)
     fun <T> accept(visitor: NodeTokenVisitor<T>): T = visitor.visitLeaf(this)
-    data class Sourced(override val type: Int, override val range: IntRange, override val source: CharStream) : LeafToken
-    data class Injected(override val type: Int, override val range: IntRange, override val source: CharStream, override val text: String) : LeafToken
 }
--- stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/data/StitcherToken.kt
+++ stitcher_antlr/src/main/kotlin/dev/kikugie/stitcher/data/StitcherToken.kt
@@ -1,12 +1,6 @@
 package dev.kikugie.stitcher.data
 
-import dev.kikugie.stitcher.util.get
-import org.antlr.v4.runtime.CharStream
-
 internal interface StitcherToken {
     val range: IntRange
-
-    val source: CharStream
-
-    val text: String get() = source[range]
+    val text: String
 }
