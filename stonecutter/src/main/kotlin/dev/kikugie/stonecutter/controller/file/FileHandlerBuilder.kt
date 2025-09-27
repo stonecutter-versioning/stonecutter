d3dc71d1bc151fd421c74697dc28d195fc817a41
kikugie <stormikiku@gmail.com>
2025-09-28 00:29:24 +0200

    Document ScannerBuilder and FileHandlerBuilder strategies

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/file/FileHandlerBuilder.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/file/FileHandlerBuilder.kt
@@ -16,37 +16,188 @@ import javax.inject.Inject
 @DslMarker @Retention(AnnotationRetention.BINARY)
 private annotation class FileHandlerDsl
 
+/**
+ * Provides a strategy for processing a file format.
+ */
 @StonecutterExperimentalFilesAPI @FileHandlerDsl
 public abstract class FileHandlerBuilder @Inject constructor(objects: ObjectFactory) : Named {
+    /**
+     * Provides a strategy for parsing file comments.
+     *
+     * @see ScannerBuilder
+     */
     @get:Nested public abstract val scanner: Property<ScannerBuilder>
+
+    /**
+     * Provides a strategy for adding comments to blocks of file content.
+     *
+     * The [CommentingStrategy] object receives a [String] corresponding to the block that needs to be commented out.
+     * The main responsibilities of the strategy are:
+     * - Maintain the formatting and indentation of the original content:
+     * ```java
+     * void incorrect() {
+     *     //? if false
+     * //    System.out.println("hey");
+     * }
+     *
+     * void correct() {
+     *     //? if false
+     *     //System.out.println("hey");
+     * }
+     * ```
+     * - Handle nested comments:
+     * ```java
+     * void incorrect() {
+     *     //? if false {
+     *     /*/*-- HEY --*/
+     *     System.out.println("hey");
+     *     *///?}
+     * }
+     *
+     * void correct() {
+     *     //? if false {
+     *     /*/^-- HEY --^/
+     *     System.out.println("hey");
+     *     *///?}
+     * }
+     * ```
+     *
+     * *Both of these are handled by [Commenter.JavaMultiline], but take notes for your own implementations.*
+     *
+     * @see Commenter.JavaMultiline
+     * @see Commenter.KotlinMultiline
+     */
     @get:Input public abstract val commenter: Property<CommentingStrategy>
+
+    /**
+     * Provides a strategy for handling comment removal.
+     *
+     * The [UncommentingStrategy] receives the comment body, opener and closer as separate arguments.
+     * The default value ([Uncommenter.Basic]) handles line break insertion for single-line comments,
+     * and should be suitable for most cases.
+     *
+     * A custom implementation may be necessary if handling of [commenter] transformations is needed.
+     * For example, [Uncommenter.JavaLike] handles converting `/^ ^/` placeholders to `/* */` comments
+     * produced by [Commenter.JavaMultiline].
+     */
     @get:Input public abstract val uncommenter: Property<UncommentingStrategy>
+
+    /**
+     * Provides a strategy for handling swap insertions.
+     *
+     * The [SwappingStrategy] receives the existing content and replacement value.
+     * The main implementation responsibility is to maintain the content formatting and indentation,
+     * which can be handled by the default value ([Swapper.Standard]).
+     */
     @get:Input public abstract val swapper: Property<SwappingStrategy>
 
     init {
         scanner.set(objects.newInstance<ScannerBuilder>())
         swapper.convention(Swapper.Standard)
+        uncommenter.convention(Uncommenter.Basic)
     }
 
+    /**
+     * Provides a strategy for handling swap insertions.
+     *
+     * The [SwappingStrategy] receives the existing content and replacement value.
+     * The main implementation responsibility is to maintain the content formatting and indentation,
+     * which can be handled by the default value ([Swapper.Standard]).
+     */
     public fun swap(strategy: SwappingStrategy): Unit = swapper.set(strategy)
+
+    /**
+     * Provides a strategy for adding comments to blocks of file content.
+     *
+     * The [CommentingStrategy] object receives a [String] corresponding to the block that needs to be commented out.
+     * The main responsibilities of the strategy are:
+     * - Maintain the formatting and indentation of the original content:
+     * ```java
+     * void incorrect() {
+     *     //? if false
+     * //    System.out.println("hey");
+     * }
+     *
+     * void correct() {
+     *     //? if false
+     *     //System.out.println("hey");
+     * }
+     * ```
+     * - Handle nested comments:
+     * ```java
+     * void incorrect() {
+     *     //? if false {
+     *     /*/*-- HEY --*/
+     *     System.out.println("hey");
+     *     *///?}
+     * }
+     *
+     * void correct() {
+     *     //? if false {
+     *     /*/^-- HEY --^/
+     *     System.out.println("hey");
+     *     *///?}
+     * }
+     * ```
+     *
+     * *Both of these are handled by [Commenter.JavaMultiline], but take notes for your own implementations.*
+     *
+     * @see Commenter.JavaMultiline
+     * @see Commenter.KotlinMultiline
+     */
     public fun comment(strategy: CommentingStrategy): Unit = commenter.set(strategy)
+
+    /**
+     * Provides a strategy for handling comment removal.
+     *
+     * The [UncommentingStrategy] receives the comment body, opener and closer as separate arguments.
+     * The default value ([Uncommenter.Basic]) handles line break insertion for single-line comments,
+     * and should be suitable for most cases.
+     *
+     * A custom implementation may be necessary if handling of [commenter] transformations is needed.
+     * For example, [Uncommenter.JavaLike] handles converting `/^ ^/` placeholders to `/* */` comments
+     * produced by [Commenter.JavaMultiline].
+     */
     public fun uncomment(strategy: UncommentingStrategy): Unit = uncommenter.set(strategy)
 
+    /**
+     * Provides a strategy for parsing file comments.
+     *
+     * @see ScannerBuilder
+     */
     public fun scanner(action: ScannerBuilder.() -> Unit): Unit = scanner.get().action()
 
     public object Commenter {
+        /**
+         * Wraps the text in `/* */`-style comments, replacing nested cases with `/^ ^/` placeholders.
+         */
         @JvmField public val JavaMultiline: CommentingStrategy = StarCommentStrategy(true)
+
+        /**
+         * Wraps the text in `/* */`-style comments, but doesn't insert `/^ ^/` placeholders like [JavaMultiline]
+         * because Kotlin handles comment depth.
+         */
         @JvmField public val KotlinMultiline: CommentingStrategy = StarCommentStrategy(false)
     }
 
     public object Uncommenter {
+        /**
+         * Removes single- and multi-line comments, handling the `/^ ^/` placeholders produced by [Commenter.JavaMultiline].
+         */
         @JvmField public val JavaLike: UncommentingStrategy = StarCommentStrategy(true)
+
+        /**
+         * Removes single- and multi-line comments, inserting line breaks for the former ones.
+         */
         @JvmField public val Basic: UncommentingStrategy = UncommentingStrategy { it, _, cl ->
             if ('\r' in cl || '\n' in cl) it + cl else it
         }
     }
 
     public object Swapper {
+        /**
+         * Replaces the content with the swap value, padding it with the content's indentation.
+         */
         @JvmField public val Standard: SwappingStrategy = StandardSwapStrategy
     }
 }
--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/file/ScannerBuilder.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/controller/file/ScannerBuilder.kt
@@ -15,17 +15,72 @@ import kotlin.reflect.KClass
 @DslMarker @Retention(AnnotationRetention.BINARY)
 private annotation class ScannerDsl
 
+/**
+ * Provides a strategy for parsing comments in a file.
+ *
+ * ### Parsing workflow
+ * As the first parsing step, Stonecutter needs to differentiate what is a comment in a file.
+ * This is represented as a token stream in the following sequence:
+ * ```
+ * <Content> <Comment-Start> <Comment-Body> <Comment-End> ...
+ * ```
+ * Since the token stream is monotone - after `<Content>` always comes `<Comment-Start>`,
+ * and after `<Comment-Body>` always comes `<Comment-End>`, the lexer can only provide
+ * comment opening and closing tokens, leaving the [ScannerAdapter][dev.kikugie.stitcher.parse.adapter.ScannerAdapter]
+ * to fill in the rest.
+ *
+ * ### Lexer registration
+ * The lexer should be registered using one of the [lexer] functions,
+ * with the comment opening and closing token types added to [openers] and [closers] respectively.
+ * The lexer **must** only emit the specified tokens, as any other will result in an error.
+ * It is recommended to `-> skip` irrelevant characters instead of sending them to `-> channel(HIDDEN)`,
+ * as they are never used by the adapter, but can cause performance issues due to the token instantiation.
+ *
+ * @see <a href="https://codeberg.org/stonecutter/stonecutter/src/branch/0.8/stitcher/src/main/antlr/dev/kikugie/stitcher/antlr/scanner/SlashStyleScanner.g4>`//` and `/* */`-comment scanner</a>
+ * @see <a href="https://codeberg.org/stonecutter/stonecutter/src/branch/0.8/stitcher/src/main/antlr/dev/kikugie/stitcher/antlr/scanner/HashStyleScanner.g4">`#`-comment scanner</a>
+ */
 @StonecutterExperimentalFilesAPI @ScannerDsl
 public interface ScannerBuilder {
+    /**The lexer factory for the configured file format.*/
     @get:Input public val constructor: Property<LexerConstructor>
+
+    /**The comment opener token types.*/
     @get:Input public val openers: SetProperty<Int>
+
+    /**The comment closer token types.*/
     @get:Input public val closers: SetProperty<Int>
 
+    /**
+     * Registers a comment lexer using a dynamic [constructor].
+     *
+     * The provided [LexerConstructor] implementation **must** be stateless.
+     */
     public fun lexer(constructor: LexerConstructor): Unit = this.constructor.set(constructor)
+
+    /**
+     * Registers a comment lexer based on the provided [grammar] string.
+     */
     public fun lexer(grammar: String): Unit = lexer(InterpretedLexerConstructor(grammar))
+
+    /**
+     * Registers a comment lexer based on its [class][clazz].
+     *
+     * The provided class **must** have a public single-argument constructor accepting a [CharStream] object.
+     */
     public fun lexer(clazz: Class<out Lexer>): Unit = lexer(ReflectionLexerConstructor(clazz))
+
+    /**
+     * Registers a comment lexer based on its [class][clazz].
+     *
+     * The provided class **must** have a public single-argument constructor accepting a [CharStream] object.
+     */
     public fun lexer(clazz: KClass<out Lexer>): Unit = lexer(clazz.java)
 
+    /**
+     * Applies configuration of the given [template].
+     *
+     * @see [ScannerBuilder.Companion]
+     */
     public infix fun from(template: Template) {
         constructor.set(template.constructor)
         openers.set(template.openers)
@@ -57,18 +112,28 @@ public interface ScannerBuilder {
     )
 
     public companion object {
+        /**
+         * Matches `//` and `/* */`-style comments, with non-nested multiline comments.
+         */
         @JvmField public val Java: Template = Template(
             ::SlashStyleScanner,
             setOf(SlashStyleScanner.SLASH_COMMENT_START, SlashStyleScanner.STAR_COMMENT_START),
             setOf(SlashStyleScanner.SLASH_COMMENT_END, SlashStyleScanner.STAR_COMMENT_END),
         )
 
+        /**
+         * Matches `//` and `/* */`-style comments, with nested multiline comments.
+         * Comments inside string templates are **not** recognized.
+         */
         @JvmField public val Kotlin: Template = Template(
             { SlashStyleScanner(it).apply { nestMultiLineComments = true } },
             setOf(SlashStyleScanner.SLASH_COMMENT_START, SlashStyleScanner.STAR_COMMENT_START),
             setOf(SlashStyleScanner.SLASH_COMMENT_END, SlashStyleScanner.STAR_COMMENT_END),
         )
 
+        /**
+         * Matches `#`-style comments.
+         */
         @JvmField public val Properties: Template = Template(
             ::HashStyleScanner,
             setOf(HashStyleScanner.HASH_COMMENT_START),
