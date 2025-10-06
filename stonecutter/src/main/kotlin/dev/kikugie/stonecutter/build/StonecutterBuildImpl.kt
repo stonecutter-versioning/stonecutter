2daba64d711f824ec22e041fb5200a352f365dca
kikugie <stormikiku@gmail.com>
2025-10-06 11:49:06 +0200

    Reorder sources in generate task for correct precedence

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/build/StonecutterBuildImpl.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/build/StonecutterBuildImpl.kt
@@ -74,7 +74,7 @@ internal abstract class StonecutterBuildImpl(val project: Project, private val p
 
         val generateTask = tasks.registerGenerateTask(src) {
             duplicatesStrategy = DuplicatesStrategy.INCLUDE
-            from(prepareTask.map { it.destination }, sharedSource)
+            from(sharedSource, prepareTask.map { it.destination })
             exclude { !it.isDirectory && it.relativePath.getFile(localSource).exists() }
             into(tasks.generatedSourcesDir.resolve(src.name))
         }
