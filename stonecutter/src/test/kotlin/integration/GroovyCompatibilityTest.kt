2f8bd9cf5f2f725bb00436c93d10592b7fbd2bf8
kikugie <stormikiku@gmail.com>
2025-11-30 15:24:34 +0100

    Add `use_parameters_function` integration test to `GroovyCompatibilityTest`.

--- stonecutter/src/test/kotlin/integration/GroovyCompatibilityTest.kt
+++ stonecutter/src/test/kotlin/integration/GroovyCompatibilityTest.kt
@@ -3,6 +3,7 @@ package integration
 import gradle.GradleTest
 import gradle.should
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.string.shouldContain
 
 class GroovyCompatibilityTest : GradleTest, ShouldSpec({
     context("settings") {
@@ -10,5 +11,14 @@ class GroovyCompatibilityTest : GradleTest, ShouldSpec({
         should("use implicit shared") { _, build -> build.run("stonecutterGenerate") }
         should("use explicit shared") { _, build -> build.run("stonecutterGenerate") }
         should("declare branch versions") { _, build -> build.run(":subproject:3:stonecutterGenerate") }
+    }
+
+    context("controller") {
+        should("use parameters function") { _, build ->
+            with(build.run().output) {
+                shouldContain("Parameters for :1")
+                shouldContain("Parameters for :2")
+            }
+        }
     }
 })
