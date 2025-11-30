718ff13a00881f1b74dc02a44e3ec8d188fe5ff5
kikugie <stormikiku@gmail.com>
2025-11-30 16:32:06 +0100

    Rename `SyntaxTest` to `OldRandomTest`

--- stonecutter/src/test/kotlin/integration/OldRandomTest.kt
+++ stonecutter/src/test/kotlin/integration/OldRandomTest.kt
@@ -10,7 +10,7 @@ import io.kotest.matchers.string.*
 import kotlin.io.path.useLines
 
 @Deprecated("Should be migrated to dedicated tests")
-class SyntaxTest : GradleTest, FreeSpec({
+class OldRandomTest : GradleTest, FreeSpec({
     "simple version" - { _, build ->
         build.run(":1:run").output shouldContain "Hello world!"
         build.run(":2:run").output shouldNotContain "Hello world!"
