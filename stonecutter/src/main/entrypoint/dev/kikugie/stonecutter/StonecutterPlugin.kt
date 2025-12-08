46e522a904ceac56d79c70538c424b09c077cc2e
kikugie <stormikiku@gmail.com>
2025-12-08 16:42:03 +0100

    Bump Stonecutter version to 0.8-beta.1 and update version catalog to 1.4.3.

--- stonecutter/src/main/entrypoint/dev/kikugie/stonecutter/StonecutterPlugin.kt
+++ stonecutter/src/main/entrypoint/dev/kikugie/stonecutter/StonecutterPlugin.kt
@@ -23,7 +23,7 @@ public abstract class StonecutterPlugin @Inject constructor(
 ) : Plugin<ExtensionAware> {
     public companion object {
         /**Current Stonecutter version.*/ // Updated by ':updateVersion' task during build
-        public const val VERSION: String = "0.8-alpha.14"
+        public const val VERSION: String = "0.8-beta.1"
     }
 
     /**
